1a568490c0a69f9ab917e5bbdd41069f24fc8bda
vidyakekan <vidyakekan79@gmail.com>
2024-02-25 22:02:55 +0530

    wad added

---
+++ exp/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res) => {
+    res.sendFile(__dirname+"/public/home.html")
+})
+
+app.get("/about",(req,res) => {
+    res.sendFile(__dirname+"/public/about.html")
+})
+
+app.get("/login",(req,res) => {
+    res.sendFile(__dirname+"/public/login.html")
+})
+
+app.get("/contact",(req,res) => {
+    res.sendFile(__dirname+"/public/contact.html")
+})
+
+app.listen(4000);
